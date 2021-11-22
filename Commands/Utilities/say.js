feee05dda2f37d0f45b3dc0d2427ac93c0fba059
Martijn Miedema <martijnmiedema01@gmail.com>
2021-11-22 22:48:05 +0100

    say command

---
+++ Commands/Utilities/say.js
@@ -0,0 +1,28 @@
+const { CommandInteraction } = require("discord.js");
+
+module.exports = {
+    name: "say",
+    description: "Make the bot say anything you want.",
+    permission: "ADMINISTRATOR",
+    options: [
+        {
+            name: "say",
+            description: "Type whatever you want to say.",
+            type: "STRING",
+            required: true
+        }
+    ],
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     */
+    execute(interaction) {
+
+        interaction.reply({content: "Will do boss.", ephemeral: true})
+        
+        const say = interaction.options.getString("say");
+
+        interaction.channel.send({content: `${say}`})
+
+    }
+}
