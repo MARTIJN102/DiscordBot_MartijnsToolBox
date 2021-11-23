4b4a95e598b394fdd1a905227f0a7cb18c6ac619
Martijn Miedema <martijnmiedema01@gmail.com>
2021-11-23 01:52:27 +0100

    replace line

--- Commands/Developer/Fun/insult.js
+++ Commands/Developer/Fun/insult.js
@@ -16,9 +16,9 @@ module.exports = {
      * @param {CommandInteraction} interaction 
      */
     async execute(interaction) {
-
-        const response = await axios.get(`https://evilinsult.com/generate_insult.php?lang=en&amp;type=json`);
+        
         const Target = interaction.options.getMember('target');
+        const response = await axios.get(`https://evilinsult.com/generate_insult.php?lang=en&amp;type=json`);
 
         interaction.reply({ content: `${Target}, ${response.data}` })
     }
